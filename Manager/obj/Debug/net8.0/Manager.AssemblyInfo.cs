01d0b1371746a6b6a6d450ed043bbb4acd5ced63
Mohammed amaan <amaanvisiontech@gmail.com>
2024-05-29 17:35:58 +0530

    commit to dll files

--- Manager/obj/Debug/net8.0/Manager.AssemblyInfo.cs
+++ Manager/obj/Debug/net8.0/Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b1448fb9ce1f0c7455aed0124ba9edc806250a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29f3e6b60d62e9d0e708a437bfbaa1b44d70c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
